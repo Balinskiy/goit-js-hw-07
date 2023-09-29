56a97f24cc1bd3c73582da6fdae4362e59c2a3a5
Balinskiy <3879342@gmail.com>
2023-09-29 16:54:52 +0300

    Update-task-01

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const container = document.querySelector('.gallery');
+
+container.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
+container.addEventListener('click', handlerClick);
+
+function createMarkup(galleryItems) {
+  return galleryItems.map(({ preview, original, description }) => `
+      <li class="gallery__item js-image-item">
+        <a class="gallery__link" href="${original}">
+          <img class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}" />
+        </a>
+      </li>
+    `).join('');
+}
+
+function handlerClick(event) {
+    event.preventDefault();
+    if (event.target === event.currentTarget) {
+        return;
+   }
+    const currentImage = event.target.closest('.js-image-item');
+    const originalImageURL = currentImage.querySelector('img').getAttribute('data-source');
+    const imageDescription = currentImage.querySelector('img').getAttribute('alt');
+
+    const instance = basicLightbox.create(`
+      <div class="modal">
+        <img src="${originalImageURL}" alt="${imageDescription}" />
+      </div>
+    `);
+
+    instance.show(); 
+}
+
+
+
+
