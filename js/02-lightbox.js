acca246ba2f2b0768baad8d2bff4ebdaf3d17d22
Balinskiy <3879342@gmail.com>
2023-09-30 13:26:57 +0300

    Update-task-02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const container = document.querySelector('.gallery');
 
-console.log(galleryItems);
+container.insertAdjacentHTML('beforeend', createMarkup(galleryItems));
+container.addEventListener('click', handlerClick);
+
+function createMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => `
+  <li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>
+    `).join('');
+}
+
+function handlerClick(event) {
+    event.preventDefault();
+    if (event.target === event.currentTarget) {
+        return;
+    }
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+
